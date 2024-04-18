0a65d4c1ccddeb91c35c246980f198854522eef9
Joe Lyles <lylesjoe@protonmail.com>
2024-04-18 05:23:49 -0400

    added admin link to home page for refresh testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <p>Home Page</p>
+      
+        <Link to="/admin">Admin</Link>
+      
     </div>
   )
 }
